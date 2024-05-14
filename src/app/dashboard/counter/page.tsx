167fe889ac37ff21f3d7490de6f3e54ae9a9c566
Juan Manuel Cavilla <juanmcavilla.dev@gmail.com>
2024-05-14 16:14:38 -0300

    Fin-seccion-5

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,20 @@
+import { CartCounter } from "@/shopping-cart/components/CartCounter";
+
+export const metadata = {
+  title: 'Shopping Cart',
+  description: 'Un simple contador'
+}
+
+export default function NamePage() {
+
+  
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      
+      <CartCounter value={ 20 } />
+      
+    </div>
+  );
+}
---
+++ src/pokemons/components/PokemonGrid.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SinglePokemon } from '../interfaces/simple-pokemon'
+import { PokemonCard } from './PokemonCard'
+
+interface Props {
+    pokemons: SinglePokemon[]
+}
+export const PokemonGrid = ({ pokemons }: Props) => {
+    return (
+        <div className="flex flex-wrap gap-10 items-center justify-center">
+            {
+                pokemons.map(pokemon => (
+                    <PokemonCard key={ pokemon.id } pokemon={pokemon}/>
+                ))
+            }
+
+        </div>
+    )
+}
